df40982e3e43fee4517d2cfe61daba24f70c9ada
David Lew <d.lperez@alumnos.upm.es>
2024-04-16 10:18:14 +0200

    resolved issue#X4

--- src/main/java/com/practica/ems/covid/Localizacion.java
+++ src/main/java/com/practica/ems/covid/Localizacion.java
@@ -9,6 +9,8 @@ import com.practica.excecption.EmsLocalizationNotFoundException;
 import com.practica.genericas.FechaHora;
 import com.practica.genericas.PosicionPersona;
 
+import static com.practica.ems.covid.ContactosCovid.getFechaHora;
+
 public class Localizacion {
 	LinkedList<PosicionPersona> lista;
 
@@ -110,17 +112,7 @@ public class Localizacion {
 	}
 	
 	private  FechaHora parsearFecha (String fecha, String hora) {
-		int dia, mes, anio;
-		String[] valores = fecha.split("\\/");
-		dia = Integer.parseInt(valores[0]);
-		mes = Integer.parseInt(valores[1]);
-		anio = Integer.parseInt(valores[2]);
-		int minuto, segundo;
-		valores = hora.split("\\:");
-		minuto = Integer.parseInt(valores[0]);
-		segundo = Integer.parseInt(valores[1]);
-		FechaHora fechaHora = new FechaHora(dia, mes, anio, minuto, segundo);
-		return fechaHora;
+		return getFechaHora(fecha, hora);
 	}
 	
 }
